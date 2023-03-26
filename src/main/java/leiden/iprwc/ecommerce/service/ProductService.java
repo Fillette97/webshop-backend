ff69419b39ad2f7a8593af66e47e960f6ae874b3
Fillette <fillettehalley@gmail.com>
2023-03-26 21:17:00 +0200

    changed get method all products

--- src/main/java/leiden/iprwc/ecommerce/service/ProductService.java
+++ src/main/java/leiden/iprwc/ecommerce/service/ProductService.java
@@ -2,9 +2,13 @@ package leiden.iprwc.ecommerce.service;
 
 import leiden.iprwc.ecommerce.entity.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
     void delete(Long product_id);
 
     void saveOrUpdate(Product product);
+
+    List<Product> findAllProducts();
 }
